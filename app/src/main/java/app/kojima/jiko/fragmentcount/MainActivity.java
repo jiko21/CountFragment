8c59276190ede765586af3ca17f88326a03c5dad
jiko21 <daikikojima0531@icloud.com>
2019-12-02 15:36:28 +0900

    Initialize commit

---
+++ app/src/main/java/app/kojima/jiko/fragmentcount/MainActivity.java
@@ -0,0 +1,14 @@
+package app.kojima.jiko.fragmentcount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
